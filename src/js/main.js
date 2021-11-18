b5db657b52afffa7306209539caef80f82f7954c
Voanarges <dmitriylyba@gmail.com>
2021-11-18 10:09:40 +0300

    ie 10-11

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-import sliders from "./modules/sliders";
-
-// import description from './modules/description';
-'use strict';
-window.addEventListener('DOMContentLoaded', ()=>{
-
-    sliders('.quastions__card', '.quastions__rightbtn');
-    
-});
